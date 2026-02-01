44acd0c84ea5d9e11afaee36b458ef52740bf651
Praneeth Meda <praneethmeda56@gmail.com>
2026-02-01 09:02:30 +0530

    Make VITE_CLIENT_ID and VITE_HOST optional

--- vite/env.ts
+++ vite/env.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 import { REDIRECT_URL_ENDPOINT } from "../src/helpers/redirect-url";
 
 const frontendSchema = z.object({
-  VITE_CLIENT_ID: z.string(),
-  VITE_HOST: z.string(),
+  VITE_CLIENT_ID: z.string().optional(),
+  VITE_HOST: z.string().optional(),
 });
 
 export const frontendCredentials = () => frontendSchema.parse(import.meta.env);
 
 export const makeRedirectUriFrontend = (reset: boolean = false) => {
-  return `${
-    frontendCredentials().VITE_HOST
-  }${REDIRECT_URL_ENDPOINT}?reset=${reset}`;
+  const credentials = frontendCredentials();
+  const host = credentials.VITE_HOST || window.location.origin;
+  return `${host}${REDIRECT_URL_ENDPOINT}?reset=${reset}`;
 };
